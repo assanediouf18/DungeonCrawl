2807a53f11b0a622d498f533ce06853a00f74c16
adiouf <h8799r@gmail.com>
2023-10-16 10:10:00 +0200

    Messages to move ok

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -5,10 +5,9 @@ use crate::prelude::*;
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
-    #[resource] turn_state: &mut TurnState
+    #[resource] turn_state: &mut TurnState,
+    commands: &mut CommandBuffer,
 ) {
     if let Some(key) = key {
         let delta = match key {
@@ -16,20 +15,21 @@ pub fn player_input(
             VirtualKeyCode::Right => Point::new(1, 0),
             VirtualKeyCode::Up => Point::new(0, -1),
             VirtualKeyCode::Down => Point::new(0, 1),
-            _ => Point::new(0, 0)
+            _ => Point::new(0, 0),
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query()
-                .filter(component::<Player>());
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            })
-        }
+        let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+        
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+            *turn_state = TurnState::PlayerTurn;
+        });
     }
 }
