1126194e4c392abc22a48d6d330c7164e43eedb7
adiouf <h8799r@gmail.com>
2023-10-16 10:35:03 +0200

    Preparing to add the hud display

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ mod spawner;
 mod systems;
 mod turn_state;
 
-//Reprendre page 135
-
 mod prelude {
     pub use std::cmp;
     pub use crate::world::SubWorld;
@@ -37,8 +35,10 @@ fn main() -> BError {
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
+        .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png")
         .build()?;
 
     main_loop(context, State::new())
@@ -83,6 +83,8 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         ctx.set_active_console(2);
+        ctx.cls();
+        ctx.set_active_console(3);
         ctx.cls();
         self.resources.insert(ctx.key);
 
